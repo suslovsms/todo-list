33927c6c68ada53ef5cd1522b5fed47378a5c4ce
maksim <suslovsms@yandex.ru>
2024-07-01 23:03:22 +0300

    Lesson_2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import React from 'react';
+import Banner from "./components/banner/banner";
+
+function Footer() {
+  return (
+    <footer>
+      Подвал сайта
+    </footer>
+  );
+}
+
+
 function App() {
   return (
     <div>
       Picture shop
+      <Banner/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/banner/banner.js
@@ -0,0 +1,8 @@
+export default function Banner(){
+    return (
+      <figure>
+        <img alt="Текст акции"/>
+        <figcaption>Текст акции</figcaption>
+      </figure>  
+    );
+}
