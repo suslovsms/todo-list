a1951b437a29f42efe5830499efb63d3c1403ed6
maksim <suslovsms@yandex.ru>
2024-10-06 12:31:22 +0300

    New website

--- src/components/bubble/bubble.js
+++
@@ -1,9 +0,0 @@
-import bubble from "./bubble.module.css"
-
-export default function Bubble(){
-    return (
-        <div className={bubble.container}>
-            <span className={bubble.bubble}></span>
-        </div>
-    );
-}
--- src/pictures.js
+++ src/pictures.js
@@ -1,60 +1,54 @@
-export default [
-    {id:1, 
-    year_of_made:"09.06.1994", 
-    end_date:"09.06.2021", 
-    tag: ["Japanese"],
-    title: "Honda",
-    description: "Some text",  
-    price: 100000, 
-    is_done: true,
-    masters: ["Ivan"," Alex"],
-    path: "/images/honda.webp"},
-
-    {id:2, 
-    year_of_made:"01.01.1977", 
-    end_date:"03.05.2022", 
-    tag: ["America"],
-    title: "Harley Davidson",
-    description: "Some text",  
-    price: 200000, 
-    is_done: true,
-    masters: ["Maksim ","Zoy ","Leon" ],
-    path: "/images/Harley_davidson.jpeg"},
-
-    {id:3, 
-    year_of_made:"09.05.1945", 
-    end_date:"11.10.2026", 
-    tag: ["Germany"],
-    title: "BMW",
-    description: "Some text",  
-    price: 300000, 
-    is_done: false,
-    masters: ["Zoy ","Leon "],
-    path: "/images/BMW.jpeg"},
-
-    {id:4, 
-    year_of_made: "19.11.1980",
-    end_date:"01.01.2024",
-    tag: ["Britan"],
-    title: "Triumph",
-    description: "Some text",  
-    price: 40000, 
-    is_done: true,
-    masters: ["Ivan ","Leon " ],
-    path: "/images/Triumph.jpg"},
-
-    {id:5, 
-    year_of_made:"04.03.1964", 
-    end_date:"21.09.2024", 
-    tag: ["USSR"],
-    title: "Ural",
-    description: "Some text",  
-    price: 500000, 
-    is_done: false,
-    masters: ["Maksim "],
-    path: "/images/URAL.jpg"},
+const cards = [
+    {
+        "id": 1,
+        "date": "01.03.2025",
+        "end_date": "07.03.2025",
+        "title": "Legendary 30",
+        "description": "Какое-то описание",
+        "price": 15000,
+        "instructor": "Kristina",
+        "path": "/images/legendary30.jpg"
+    },
+    {
+        "id": 2,
+        "date": "15.04.2025",
+        "end_date": "22.04.2025",
+        "title": "Kazbegi",
+        "description": "Какое-то описание",
+        "price": 12000,
+        "instructor": "Alexander",
+        "path": "/images/Kazbegi.jpg"
+    },
+    {
+        "id": 3,
+        "date": "10.05.2025",
+        "end_date": "17.05.2025",
+        "title": "Chegem",
+        "description": "Какое-то описание",
+        "price": 20000,
+        "instructor": "Elena",
+        "path": "/images/chegem.jpeg"
+    },
+    {
+        "id": 4,
+        "date": "20.06.2025",
+        "end_date": "27.06.2025",
+        "title": "Belyha",
+        "description": "Какое-то описание",
+        "price": 18000,
+        "instructor": "Dmitry",
+        "path": "/images/Belyha.webp"
+    },
+    {
+        "id": 5,
+        "date": "05.08.2025",
+        "end_date": "12.08.2025",
+        "title": "Kamchatka",
+        "description": "Какое-то описание",
+        "price": 25000,
+        "instructor": "Svetlana",
+        "path": "/images/Kamchatka.jpg"
+    }
 ]
 
-//  Отрисовать все
-// задачи, которые не были завершены в отсортированном по дате
-// завершению виде (сначала срочные).
+export default cards;
